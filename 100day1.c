258cb3a00526c55ec0ba77cf6d353cf5e723dca5
Shreyanshhh28 <jshreyansh281106@gmail.com>
2025-09-14 22:32:26 +0530

    Update 100day1.c

--- 100day1.c
+++ 100day1.c
@@ -3,14 +3,12 @@
 
 int main() {
     int a, b;
-    // Input two integers
     scanf("%d %d", &a, &b);
-    // Calculate results
     int sum = a + b;
     int diff = a - b;
     int product = a * b;
     int quotient = a / b;
-    // Output in required format
     printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n", sum, diff, product, quotient);
     return 0;
 }
+
