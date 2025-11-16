0f5ea5a4baf10b534523a3afa0ee80bce906316f
Shreyanshhh28 <jshreyansh281106@gmail.com>
2025-11-16 16:42:51 +0530

    Add files via upload

---
+++ 100days34(1).c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, pos, elem;
+ 
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n + 1]; 
+    printf("Enter elements of array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+   
+    printf("Enter position and element to insert: ");
+    scanf("%d %d", &pos, &elem);
+    for (int i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+    
+    arr[pos] = elem; 
+    n++; 
+
+    printf("Array after insertion: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
---
+++ 100days35(1).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+   
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter elements of array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int largest = arr[0], secondLargest = arr[0];
+   
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    
+    printf("Second largest element: %d\n", secondLargest);
+    
+    return 0;
+}
